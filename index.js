ff874970f92d4cfeccc38a47ca045ed7b0da4b66
wilhelmson <wilhelmson@el-xps.(none)>
2014-01-08 11:49:48 -0800

    tabs to spaces

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var slice = Array.prototype.slice;
 
 module.exports = function maybe (fn){
-	return function(){
-		var args = slice.call(arguments),
-			valid,
-			result;
-		
-		valid = args.reduce(function(curr, prev){
-			return curr && prev != null;
-		}, true);
+    return function(){
+        var args = slice.call(arguments),
+            valid,
+            result;
+        
+        valid = args.reduce(function(curr, prev){
+            return curr && prev != null;
+        }, true);
 
-		if(!valid) return void 0;
-		
-		result = fn.apply(null, args);
-		
-		return typeof result === 'function' ? maybe(result) : result;
-	}
+        if(!valid) return void 0;
+        
+        result = fn.apply(null, args);
+        
+        return typeof result === 'function' ? maybe(result) : result;
+    }
 }
--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
 var test = require('tap').test,
-	maybe = require('../'),
-	add = function(a,b){
-		return a + b
-	},
-	mayAdd = maybe(add);
+    maybe = require('../'),
+    add = function(a,b){
+        return a + b
+    },
+    mayAdd = maybe(add);
 
 test('throws ReferenceError if argument is null', function(t){
-	var result = mayAdd(null,1);
-	t.plan(1);
+    var result = mayAdd(null,1);
+    t.plan(1);
 
-	t.equals(void 0, result);
+    t.equals(void 0, result);
 });
